149a61bedb6188cc908e400712c6d0d07d3565bb
Boyonelove <101815160+Boyonelove@users.noreply.github.com>
2024-12-13 10:22:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 import streamlit as st
-from dotenv import load_dotenv
 from streamlit_option_menu import option_menu
-import os
-import sys
-
-
-sys.path.append("E:\\hk7\\5.CMPM\\chay thu")
-load_dotenv()
-
 import login
 import recommend
 
+# Cấu hình trang
 st.set_page_config(
     page_title="Hotel Recommendation",
     page_icon=":tokyo_tower:"  # Biểu tượng tòa nhà
 )
 
-# Google Analytics
-st.markdown(
-    f"""
-        <script async src="https://www.googletagmanager.com/gtag/js?id={os.getenv('analytics_tag')}"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){{dataLayer.push(arguments);}}
-            gtag('js', new Date());
-            gtag('config', '{os.getenv("analytics_tag")}');
-        </script>
-    """, unsafe_allow_html=True
-)
-
+# MultiApp Class để quản lý nhiều trang
 class MultiApp:
     def __init__(self):
         self.apps = []
@@ -40,22 +21,19 @@ class MultiApp:
         })
 
     def run(self, selected):
-        # Gọi hàm tương ứng với trang đã chọn
         for app in self.apps:
             if app['title'] == selected:
-                app['function']()  # Gọi hàm tương ứng
+                app['function']()  # Gọi hàm của trang tương ứng
 
-# Chạy ứng dụng
+# Khởi tạo ứng dụng
 if __name__ == "__main__":
     app = MultiApp()
     app.add_app("🔒 Login", login.main)  
     app.add_app("🕵️ Recommend", recommend.main)  
 
-st.sidebar.markdown("<h2 style='font-size: 24px;'>🏨 Main Menu</h2>", unsafe_allow_html=True)
-selected = st.sidebar.selectbox("Choose a page", ["🔒 Login", "🕵️ Recommend"])
+    # Sidebar menu
+    st.sidebar.markdown("<h2 style='font-size: 24px;'>🏨 Main Menu</h2>", unsafe_allow_html=True)
+    selected = st.sidebar.selectbox("Choose a page", ["🔒 Login", "🕵️ Recommend"])
     
-    # st.sidebar.markdown("## Main Menu")
-    # selected = st.sidebar.selectbox("Choose a page", ["Login", "Recommend"])
-
-    # Gọi phương thức run() với trang đã chọn
-app.run(selected)
+    # Chạy ứng dụng với trang được chọn
+    app.run(selected)
